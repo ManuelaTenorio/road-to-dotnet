82fafbbff000f8686fbdfb3bc990a72bf80e933d
Manuela Tenorio <manuelatenorio61@gmail.com>
2026-06-30 16:27:01 -0300

    Parte 2 - tipos de variáveis em C#

---
+++ ConsoleApp2/Program2.cs
@@ -0,0 +1,17 @@
+namespace Program2;
+
+public class Program
+{
+    public static void Main()
+    {
+        string myName = "Manuela";
+        int Idade = 28;
+        double numberDouble = 1.74;
+        char character = '@';
+        bool boolean = true;
+
+
+
+        Console.WriteLine(Idade);
+    }
+}
